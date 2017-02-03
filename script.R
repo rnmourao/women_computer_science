7f33c963e51a6cad907aa68da7c92dbaa722f271
Guilherme N. Ramos <ramos@gnramos.com>
2017-02-03 16:47:27 -0200

    Ajuste do script, começo da escrita do artigo.

--- script.R
+++ script.R
@@ -6,13 +6,13 @@
 # the University of Brasília (http://meninas.cic.unb.br)
 
 # Dependencies
-for (pkg in c('readxl', 'readr', 'arules', 'ggplot2', 'reshape2',
-              'gridExtra', 'stringr', 'knitr', 'agricolae'))
+for (pkg in c('agricolae', 'ggplot2', 'knitr', 'readxl'))
   if (!require(pkg, character.only=TRUE))
     ignore <- install.packages(pkg)
 
 # Pretty printing
-options('scipen'=100, 'digits'=2)
+options('scipen'=100, 'digits'=2, device='pdf')
+cat('\n')
 
 poll.answers <- read_excel('raw.xlsx', sheet='unificado', na='')
 
@@ -20,6 +20,8 @@ poll.answers <- read_excel('raw.xlsx', sheet='unificado', na='')
 poll.answers$Q1 <- NULL
 poll.answers$Q2 <- NULL
 poll.answers$Serie <- stringr::str_trim(poll.answers$Serie, side='both')
+
+cat('Number of respondents: ', nrow(poll.answers), '\n\n')
 
 # Girls only
 poll.answers <- subset(poll.answers, poll.answers$Sexo == 'F')
@@ -42,6 +44,22 @@ poll.answers$Fara_Computacao[poll.answers$Fara_Computacao=='Nao sei ainda'] <- '
 # Processing
 poll.answers <- as.data.frame(lapply(poll.answers, as.factor))
 
+cat('Female respondents: ', nrow(poll.answers), '\n\n')
+
+# Results per year #############################################################
+respondents.per.year <- aggregate(x=list(Total=poll.answers$Ano),
+                               by=list(Year=poll.answers$Ano),
+                               FUN=length)
+
+p <- ggplot(data=respondents.per.year, aes(x='', y=Total, fill=Year)) +
+     geom_bar(width=1, stat='identity') +
+     coord_polar(theta='y', start=0) +
+     scale_x_discrete() +
+     xlab('') + ylab('') + labs(fill='') + # + labs(fill='Stage')
+     # ggtitle('Respondents per Year') +
+ggsave('dexa/img/RespondentsPerYear.pdf', width=7, height=2)
+
+
 # Results per educational stage ################################################
 educational.stage <- aggregate(x=list(total=poll.answers$Serie),
                                by=list(Year=poll.answers$Ano, Serie=poll.answers$Serie),
@@ -51,38 +69,44 @@ by.year <- aggregate(x=list(Total=poll.answers$Serie),
                      FUN=length)
 
 educational.stage <- merge(educational.stage, by.year)
-educational.stage$Percentual <- (educational.stage$total * 100) / educational.stage$Total
+educational.stage$Percentage <- (educational.stage$total * 100) / educational.stage$Total
 
-p <- ggplot(data=educational.stage, aes(x='', y=Percentual, fill=Serie)) +
+p <- ggplot(data=educational.stage, aes(x='', y=Percentage, fill=Serie)) +
      geom_bar(width=1, stat='identity') +
      facet_grid(facets=. ~ Year) +
      coord_polar(theta='y', start=0) +
      scale_x_discrete() +
      xlab('') + ylab('') + labs(fill='') + # + labs(fill='Stage')
-     ggtitle('Interviewees per Educational Stage') +
+     # ggtitle('Respondents per Educational Stage') +
      scale_fill_discrete(breaks=c('Middle School', 'High School (10th Grade)', 'High School (11th Grade)', 'High School (12th Grade)', 'Adult Education Program', 'College'))
-ggsave('EducationalStage.pdf', width=7, height=2)
+ggsave('dexa/img/EducationalStage.pdf', width=7, height=2)
 
 
 # Results per interest in undergraduate field of study #########################
-field.of.interest <- aggregate(x=list(total=poll.answers$Fara_Curso_Superior),
-                               by=list(Year=poll.answers$Ano, Fara_Curso_Superior=poll.answers$Fara_Curso_Superior),
+field.of.interest <- aggregate(x=list(NumRespondents=poll.answers$Fara_Curso_Superior),
+                               by=list(Year=poll.answers$Ano, FieldOfInterest=poll.answers$Fara_Curso_Superior),
                                FUN=length)
-by.year <- aggregate(x=list(Total=poll.answers$Fara_Curso_Superior),
+by.year <- aggregate(x=list(TotalRespondents=poll.answers$Fara_Curso_Superior),
                      by=list(Year=poll.answers$Ano),
                      FUN=length)
 
 field.of.interest <- merge(field.of.interest, by.year)
-field.of.interest$Percentual <- (field.of.interest$total * 100) / field.of.interest$Total
+field.of.interest$Percentage <- (field.of.interest$NumRespondents * 100) / field.of.interest$TotalRespondents
 
-p <- ggplot(data=field.of.interest, aes(x='', y=Percentual, fill=Fara_Curso_Superior)) +
+p <- ggplot(data=field.of.interest, aes(x='', y=Percentage, fill=FieldOfInterest)) +
      geom_bar(width=1, stat='identity') +
      facet_grid(facets=. ~ Year) +
      coord_polar(theta='y', start=0) +
      scale_x_discrete() +
      xlab('') + ylab('') + labs(fill='') + # + labs(fill='Field')
-     ggtitle('Interviewees per Field of Interest')
-ggsave('FieldOfInterest.pdf', width=7, height=2)
+     # ggtitle('Respondents per Field of Interest')
+ggsave('dexa/img/FieldOfInterest.pdf', width=7, height=2)
+
+cat('All respondents:\n')
+aggregate(x=list(Percentage=field.of.interest$Percentage),
+          by=list(FieldOfInterest=field.of.interest$FieldOfInterest),
+          FUN=mean)
+cat('\n')
 
 # Results for interest in Computer Science #####################################
 interest.in.CS <- aggregate(x=list(Quantidade=poll.answers$Fara_Computacao),
@@ -93,16 +117,16 @@ by.year <- aggregate(x=list(Total=poll.answers$Fara_Computacao),
                      FUN=length)
 
 interest.in.CS <- merge(interest.in.CS, by.year)
-interest.in.CS$Percentual <- (interest.in.CS$Quantidade * 100) / interest.in.CS$Total
+interest.in.CS$Percentage <- (interest.in.CS$Quantidade * 100) / interest.in.CS$Total
 
-p <- ggplot(data=interest.in.CS, aes(x='', y=Percentual, fill=Fara_Computacao)) +
+p <- ggplot(data=interest.in.CS, aes(x='', y=Percentage, fill=Fara_Computacao)) +
      geom_bar(width=1, stat='identity') +
      facet_grid(facets=. ~ Year) +
      coord_polar(theta='y', start=0) +
      scale_x_discrete() +
      xlab('') + ylab('') + labs(fill='') + # labs(fill='Ano')
-     ggtitle('Interviewees Interested in CS')
-ggsave('InterestInCS.pdf', width=7, height=2)
+     # ggtitle('Respondents Interested in CS')
+ggsave('dexa/img/InterestInCS.pdf', width=7, height=2)
 
 # Variance analysis ############################################################
 
@@ -111,9 +135,6 @@ CS.choice <- data.frame(Fara_Computacao=levels(poll.answers$Fara_Computacao),
                         CS_Choice=c(1, 2, 3))
 poll.answers <- merge(poll.answers, CS.choice)
 CS.choice.index <- match('CS_Choice', names(poll.answers))
-
-print('CS.choice.index')
-print(CS.choice.index)
 
 year.index <- match('Ano', names(poll.answers))
 temp <- data.frame(Tratamento=poll.answers[, year.index],
@@ -126,13 +147,12 @@ cat('\n')
 knit_print(HSD.test(fit, 'Tratamento'))
 
 par(mfrow=c(2,2))
-
-pdf('ANOVA.pdf')
+pdf('dexa/img/ANOVA.pdf')
 plot(fit)
-dev.off()
+ignore <- dev.off()
 
 # Other attributes
-pdf('Variance.pdf')
+pdf('dexa/img/Variance.pdf')
 for (i in (year.index + 1):(CS.choice.index-1)) {  # CS.choice.index is the last one
   temp <- data.frame(Tratamento=poll.answers[, i], CS_Choice=poll.answers[, CS.choice.index])
 
@@ -152,4 +172,4 @@ for (i in (year.index + 1):(CS.choice.index-1)) {  # CS.choice.index is the last
 
   plot(fit)
 }
-dev.off()
+ignore <- dev.off()
