baee98fc2623825ff372a1114a31c5b1ec5ea6cb
rnmourao <robertonunesmourao@yahoo.com.br>
2017-02-22 00:29:29 -0300

    check assumptions for anova test as guidance for charts

--- src/script.R
+++ src/script.R
@@ -7,12 +7,9 @@
 
 # Dependencies
 library(agricolae)
-library(arules)
-library(arulesViz)
 library(ggplot2)
 library(gridExtra)
 library(readxl)
-library(reshape)
 library(stringr)
 
 # Pretty printing
@@ -23,7 +20,7 @@ cat('\n')
 rm(list=ls())
 
 #### Set working directory
-# setwd('/home/f8676628/Documentos/women_computer_science/src/')
+# setwd('/home/mourao/Documentos/women_computer_science/src/')
 plot.dir <- '../dexa/img/'
 
 # Get raw data
@@ -80,7 +77,6 @@ p <- ggplot(data=educational.stage, aes(x='', y=Percentage, fill=Educational_Sta
                                   'High School (11th Grade)', 'High School (12th Grade)',
                                   'Adult Education Program', 'College'))
 ggsave(paste0(plot.dir, 'EducationalStage.pdf'), width=7, height=2)
-
 
 # Results per interest in undergraduate field of study #########################
 field.of.interest <- aggregate(x=list(NumRespondents=poll.answers$Field_Interest),
@@ -144,7 +140,7 @@ CS.response.index <- match('CS_Interest', names(poll.answers))
 
 year.index <- match('Year', names(poll.answers))
 
-treatments <- data.frame(treatment=NULL, f.value=NULL, max.mean=NULL, sup=NULL)
+treatments <- data.frame(treatment=NULL, f.value=NULL, max.mean=NULL, assumptions=NULL)
 
 # Analyze individual attributes
 for (i in year.index:(CS.choice.index-1)) {  # CS.choice.index is the last one
@@ -152,10 +148,46 @@ for (i in year.index:(CS.choice.index-1)) {  # CS.choice.index is the last one
                      CS_Choice=poll.answers[, CS.choice.index],
                      CS_Interest=poll.answers[, CS.response.index])
   attribute.name <- colnames(poll.answers)[i]
-
+  
   f <- as.formula('CS_Choice ~ Treatment')
   fit <- aov(f, data=temp)
   fit.summary <- summary(fit)
+  
+  ## checking anova assumptions
+  
+  # Are residuals normal? Since normality assumption is not a big issue when treatments are fixed
+  # effects, we will test it using a rough test of Skewness and Kurtosis instead usual 
+  # normality tests, e.g., Shapiro-Wilk and Kolmogorov-Smirnov.
+  # using rule of thumb proposed by Bulmer, 1979.  
+  symmetric <- (abs(skewness(fit$residuals)) < 0.5)
+  # using rule of thumb proposed by Vieira, 2006.
+  not_leptokurtic <- kurtosis(fit$residuals) <= 0
+  
+  # Are samples independent each other? Yes, because each observation belongs to one student.
+  independent <- TRUE
+  
+  # Are variances equal?
+  # using rule of thumb proposed by Dean and Voss - Design and analysis of experiments, 1999.
+  v <- aggregate(x=list(variance=temp$CS_Choice), by=list(Treatment=temp$Treatment), FUN=var)
+  if (max(v$variance, na.rm = TRUE) / min(v$variance, na.rm = TRUE) < 3) {
+    homoscedastic <- TRUE
+  } else {
+    homoscedastic <- FALSE
+  }
+  
+  # cannot use aov for unbalanced designs...
+  if (any(is.na(v$variance))) { 
+    balanced <- FALSE
+  } else {
+    balanced <- TRUE
+  }
+  
+  if (symmetric & not_leptokurtic & independent & homoscedastic & balanced) {
+    a <- 'Granted'
+  } else {
+    a <- 'Not Granted'
+  }
+  
   tukey <- HSD.test(fit, 'Treatment')
 
   temp2 <- aggregate(x=list(Quantity=temp$CS_Interest),
@@ -166,20 +198,22 @@ for (i in year.index:(CS.choice.index-1)) {  # CS.choice.index is the last one
        scale_fill_discrete(name='Treatment')
   ggsave(paste0(plot.dir, 'plot_', attribute.name, '.pdf'), width=5, height=3)
 
-  # Qual a origem destes números mágicos (6 e 2.5)?
+  # width adjustments based on trial and error
   w <- 6 + 2.5 * nchar(as.character(fit.summary[[1]][['Pr(>F)']][1])) / 20
-  pdf(paste0(plot.dir, 'anova_table_', attribute.name, '.pdf'), height=1, width=w)
+  
+  pdf(paste0(plot.dir, 'anova_', attribute.name, '.pdf'), height=1, width=w)
     grid.table(anova(fit))
   ignore <- dev.off()
 
-  pdf(paste0(plot.dir, 'anova_chart_', attribute.name, '.pdf'))
+  pdf(paste0(plot.dir, 'conditions_', attribute.name, '.pdf'))
     par(mfrow=c(2,2))
     plot(fit)
   ignore <- dev.off()
 
-  # Qual a origem destes números mágicos (1.5 e 0.1)?
+  # height and width adjustments based on trial and error
   h <- 1.5 + 0.1 * length(unique(temp$Treatment))
   w <- 1.5 + 0.1 * max(nchar(as.character(levels(temp$Treatment))))
+  
   pdf(paste0(plot.dir, 'tukey_', attribute.name, '.pdf'), height=h, width=w)
     grid.table(tukey$groups, rows=NULL)
   ignore <- dev.off()
@@ -187,14 +221,13 @@ for (i in year.index:(CS.choice.index-1)) {  # CS.choice.index is the last one
   groups <- tukey$groups
   means <- tukey$means
   sum.means <- sum(means$r)
-  # Qual a origem deste número mágico (9)?
-  max.mean <- -9
-  s <- 0
+  
+  # initialization: max.mean begins with lower possible value for mean of CS_Choice attribute
+  max.mean <- -1
   for (j in 1:nrow(groups)) {
     if (groups$means[j] > max.mean) {
       if (str_count(paste(groups$M, collapse=''), as.character(groups$M[j])) == 1) {
         max.mean <- groups$means[j]
-        s <- means$r[row.names(means) == trimws(groups$trt[j])] / sum.means
       }
     }
   }
@@ -202,7 +235,7 @@ for (i in year.index:(CS.choice.index-1)) {  # CS.choice.index is the last one
   df <-  data.frame(treatment=attribute.name,
                     f.value=fit.summary[[1]][['F value']][[1]],
                     max.mean=max.mean,
-                    sup=s)
+                    assumptions=a)
   treatments <- rbind(treatments, df)
 }
 
@@ -236,9 +269,9 @@ while(!gotcha | ncol(temp) <= 1) {
 }
 
 ### Test interactions
-CS.choice.index <- match('CS_Choice', names(temp))
 combinations <- t(combn(1:(ncol(temp) - 1), 2))
 for (i in 1:nrow(combinations)) {
+  CS.choice.index <- match('CS_Choice', names(temp))
   temp2 <- temp[, c(combinations[i, 1], combinations[i, 2], CS.choice.index)]
   temp2 <- temp2[!is.na(temp2[, 1]) & !is.na(temp2[, 2]),]
   temp2$interaction <- as.factor(paste0(temp2[, 1], '_x_', temp2[, 2]))
@@ -248,19 +281,63 @@ for (i in 1:nrow(combinations)) {
   f <- as.formula(paste("CS_Choice ~", trt))
   fit <- aov(f, data=temp2)
   fit.summary <- summary(fit)
+  
+  ## checking anova assumptions
+  
+  # Are residuals normal? Since normality assumption is not a big issue when treatments are fixed
+  # effects, we will test it using a rough test of Skewness and Kurtosis instead usual 
+  # normality tests, e.g., Shapiro-Wilk and Kolmogorov-Smirnov.
+  # using rule of thumb proposed by Bulmer, 1979.  
+  symmetric <- (abs(skewness(fit$residuals)) < 0.5)
+  # using rule of thumb proposed by Vieira, 2006.
+  not_leptokurtic <- kurtosis(fit$residuals) <= 0
+  
+  # Are samples independent each other? Yes, because each observation belongs to one student.
+  independent <- TRUE
+  
+  # Are variances equal?
+  # using rule of thumb proposed by Dean and Voss - Design and analysis of experiments, 1999.
+  CS.choice.index <- match('CS_Choice', names(temp2))
+  Treatment.index <- match(trt, names(temp2))
+  v <- aggregate(x=list(variance=temp2[, CS.choice.index]), 
+                 by=list(Treatment=temp2[, Treatment.index]), FUN=var)
+  if (max(v$variance, na.rm = TRUE) / min(v$variance, na.rm = TRUE) < 3) {
+    homoscedastic <- TRUE
+  } else {
+    homoscedastic <- FALSE
+  }
+  
+  # cannot use aov for unbalanced designs...
+  if (any(is.na(v$variance))) { 
+    balanced <- FALSE
+  } else {
+    balanced <- TRUE
+  }
+    
+  if (symmetric & not_leptokurtic & independent & homoscedastic & balanced) {
+    a <- 'Granted'
+  } else {
+    a <- 'Not Granted'
+  }
+  
   tukey <- HSD.test(fit, trt)
 
-  # Qual a origem destes números mágicos (4 e 2.6)?
+  # width adjustments based on trial and error
   w <- 4 + 2.6 *(nchar(trt) + nchar(as.character(summary(fit)[[1]][['Pr(>F)']][1])))/ 20
-  pdf(paste0(plot.dir, 'anova_table_', trt, '.pdf'), height=1, width=w)
+  pdf(paste0(plot.dir, 'anova_', trt, '.pdf'), height=1, width=w)
     grid.table(anova(fit))
   ignore <- dev.off()
 
-  pdf(paste0(plot.dir, 'anova_chart_', trt, '.pdf'))
+  pdf(paste0(plot.dir, 'conditions_', trt, '.pdf'))
+    par(mfrow=c(2,2))
+    plot(fit)
+  ignore <- dev.off()  
+  
+  pdf(paste0(plot.dir, 'interactions_', trt, '.pdf'))
     interaction.plot(temp2[, 1], temp2[, 2], temp2[, 3], type='b')
   ignore <- dev.off()
 
-  # Qual a origem destes números mágicos (1.5 e 0.3 e 0.1)?
+  # height and width adjustments based on trial and error
   h <- 1.5 + 0.3 * length(levels(temp2[, 4]))
   w <- 1.5 + 0.1 * max(nchar(as.character(levels(temp2[, 4]))))
   pdf(paste0(plot.dir, 'tukey_', trt, '.pdf'), height=h, width=w)
@@ -270,14 +347,13 @@ for (i in 1:nrow(combinations)) {
   groups <- tukey$groups
   means <- tukey$means
   sum.means <- sum(means$r)
-  # Qual a origem deste número mágico?
-  max.mean <- -9
-  s <- 0
+  
+  # initialization: max.mean begins with lower possible value for mean of CS_Choice attribute
+  max.mean <- -1
   for (j in 1:nrow(groups)) {
     if (groups$means[j] > max.mean) {
       if (str_count(paste(groups$M, collapse=''), as.character(groups$M[j])) == 1) {
         max.mean <- groups$means[j]
-        s <- means$r[row.names(means) == trimws(groups$trt[j])] / sum.means
       }
     }
   }
@@ -285,85 +361,9 @@ for (i in 1:nrow(combinations)) {
   df <- data.frame(treatment=trt,
                    f.value=fit.summary[[1]][['F value']][[1]],
                    max.mean=max.mean,
-                   sup=s)
+                   assumptions=a)
   treatments <- rbind(treatments, df)
 }
 
-# Qual a origem deste número mágico? (0.1)
-treatments <- treatments[treatments$sup >= 0.1,]
 treatments <- treatments[order(treatments$max.mean, decreasing=TRUE),]
 write.table(treatments, '../data/anova.csv', row.names=FALSE, dec=',', sep=';')
-
-# Association rules will be done in future works
-quit()
-################################################################################
-################################################################################
-################################################################################
-################################################################################
-
-
-
-
-
-
-
-
-
-# Association Rule Mining #############################################################
-
-# Restoring data frame to its original format
-CS.choice.index <- match('CS_Choice', names(poll.answers))
-year.index <- match('Year', names(poll.answers))
-temp <- poll.answers[, -c(CS.choice.index, year.index)]
-
-# Mining rules
-
-# APRIORI
-cs_rules = apriori(data = temp,
-                   parameter = list(confidence=0.5, maxtime=300, maxlen=3),
-                   appearance = list(rhs="CS_Interest=Yes", default="lhs"))
-cs_rules_ordered <- sort(cs_rules, by='lift')
-
-# plot
-pdf(paste0(plot.dir, 'apriori.pdf'))
-  plot(cs_rules_ordered)
-ignore <- dev.off()
-
-# Saving rules on disk
-write(cs_rules_ordered, file='../data/apriori.csv', sep=';', row.names=FALSE)
-
-# # ECLAT
-# cs_rules_eclat <- eclat(data=temp, parameter=list(maxlen=3, support=0.01))
-# cs_rules_eclat <- sort(cs_rules_eclat, by='support')
-#
-# # Checking complementary rules
-# df <- data.frame(itemset=labels(cs_rules_eclat), support=cs_rules_eclat@quality)
-# df$itemset <- gsub("\\{|\\}", "", df$itemset)
-# itemsets <- as.data.frame(str_split_fixed(gsub("\\{|\\}", "", df$itemset), ",", 3))
-# names(itemsets) <- c("item1", "item2", "item3")
-# itemsets[grepl("CS_Interest", itemsets$item2, fixed=TRUE), c("item1", "item2")] <- itemsets[grepl("CS_Interest", itemsets$item2, fixed=TRUE), c("item2", "item1")]
-# itemsets[grepl("CS_Interest", itemsets$item3, fixed=TRUE), c("item1", "item3")] <- itemsets[grepl("CS_Interest", itemsets$item3, fixed=TRUE), c("item3", "item1")]
-# itemsets <- cbind(itemsets, value=df$support)
-# itemsets <- itemsets[order(itemsets$item1, itemsets$item2, itemsets$item3),]
-# # complementary <- cast(data=itemsets[grepl("CS_Interest", itemsets$item1, fixed=TRUE),], formula="item2+item3~item1", mean, fill=NA)
-# uniques <- itemsets[itemsets$item2 == "" & itemsets$item3 == "",]
-# CS_plus_other <- itemsets[itemsets$item1 == "CS_Interest=Yes" & itemsets$item2 != "" & itemsets$item3 == "",]
-#
-# ## Rules A => B
-# # confidence (A => B) = support(A U B) / support(A)
-# # lift (A => B) = support(A U B) / support(A) * support(B)
-#
-# CS_plus_other$confidence <- NA
-# CS_plus_other$lift <- NA
-# for (i in 1:nrow(CS_plus_other)) {
-#   item <- CS_plus_other$item2[i]
-#   A <- uniques$value[match(item, uniques$item1)]
-#   B <- uniques$value[match("CS_Interest=Yes", uniques$item1)]
-#   CS_plus_other$confidence[i] <- CS_plus_other$value[i] / A
-#   CS_plus_other$lift[i] <- CS_plus_other$value[i] / (A * B)
-# }
-#
-# # Saving rules on disk
-# # write(complementary, file='../data/eclat.txt')
-#
-# # Finding interactions...
