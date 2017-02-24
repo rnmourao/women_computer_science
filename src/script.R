b68b818c90f9b3874dc62a079aa33f2b2b708878
rnmourao <robertonunesmourao@yahoo.com.br>
2017-02-24 19:32:13 -0300

    some adjustments in charts and tables

--- src/script.R
+++ src/script.R
@@ -38,6 +38,11 @@ cat('Number of respondents: ', nrow(poll.answers), '\n\n')
 poll.answers <- subset(poll.answers, poll.answers$Gender == 'F')
 poll.answers$Gender <- NULL
 
+# Only Middle and High Schools
+poll.answers <- subset(poll.answers, 
+                       poll.answers$Educational_Stage != 'College' & 
+                         poll.answers$Educational_Stage != 'Adult Education Program')
+
 # Processing
 poll.answers <- as.data.frame(lapply(poll.answers, as.factor))
 
@@ -76,8 +81,7 @@ p <- ggplot(data=educational.stage, aes(x='', y=Percentage, fill=Educational_Sta
      xlab('') + ylab('') + labs(fill='') + # + labs(fill='Stage')
      # ggtitle('Respondents per Educational Stage') +
      scale_fill_discrete(breaks=c('Middle School', 'High School (10th Grade)',
-                                  'High School (11th Grade)', 'High School (12th Grade)',
-                                  'Adult Education Program', 'College'))
+                                  'High School (11th Grade)', 'High School (12th Grade)'))
 ggsave(paste0(plot.dir, 'EducationalStage.pdf'), width=7, height=2)
 
 # Results per interest in undergraduate field of study #########################
@@ -138,7 +142,7 @@ CS.choice$CS_Choice[CS.choice$CS_Interest == 'Maybe'] <- 0
 CS.choice$CS_Choice[CS.choice$CS_Interest == 'Yes'] <- 1
 poll.answers <- merge(poll.answers, CS.choice)
 CS.choice.index <- match('CS_Choice', names(poll.answers))
-CS.response.index <- match('CS_Interest', names(poll.answers))
+CS.interest.index <- match('CS_Interest', names(poll.answers))
 
 year.index <- match('Year', names(poll.answers))
 
@@ -148,7 +152,7 @@ treatments <- data.frame(treatment=NULL, f.value=NULL, max.mean=NULL, assumption
 for (i in year.index:(CS.choice.index-1)) {  # CS.choice.index is the last one
   temp <- data.frame(Treatment=poll.answers[, i],
                      CS_Choice=poll.answers[, CS.choice.index],
-                     CS_Interest=poll.answers[, CS.response.index])
+                     CS_Interest=poll.answers[, CS.interest.index])
   temp <- temp[!is.na(temp$Treatment),]
   attribute.name <- colnames(poll.answers)[i]
   
@@ -158,8 +162,6 @@ for (i in year.index:(CS.choice.index-1)) {  # CS.choice.index is the last one
   model <- lm(formula=f, data=temp, contrasts=list(Treatment = contr.sum))
   fit <- Anova(model, type="III")
   fit.summary <- summary(model)  
-  mcs <- summary(glht(model, linfct = mcp(Treatment="Tukey")))
-  tukey <- cld(mcs, level=0.05, decreasing=TRUE)
   
   ## checking anova assumptions
   
@@ -190,41 +192,16 @@ for (i in year.index:(CS.choice.index-1)) {  # CS.choice.index is the last one
   } else {
     a <- 'Not Granted'
   }
-  
-  # barplot to show differences between treatments
-  temp2 <- aggregate(x=list(Quantity=temp$CS_Interest),
-                     by=list(CS_Interest=temp$CS_Interest, Treatment=temp$Treatment),
-                     FUN=length)
-  p <- ggplot(temp2, aes(fill=Treatment, y=Quantity, x=CS_Interest)) +
-       geom_bar(position="dodge", stat="identity") +
-       scale_fill_discrete(name='Treatment')
-  ggsave(paste0(plot.dir, 'plot_', attribute.name, '.pdf'), width=5, height=3)
-
-  # anova table
-  pdf(paste0(plot.dir, 'anova_', attribute.name, '.pdf'), height=1, width=8)
-    grid.table(fit)
-  ignore <- dev.off()
-
-  # Residuals charts
-  pdf(paste0(plot.dir, 'conditions_', attribute.name, '.pdf'))
-    residualPlots(model, layout= c(2, 1))
-    qqPlot(model)
-  ignore <- dev.off()
-
-  # height and width adjustments based on trial and error
-  h <- 1.5 + 0.1 * length(unique(temp$Treatment))
-  w <- 1.5 + 0.1 * max(nchar(as.character(levels(temp$Treatment))))
 
   # tukey-kramer post hoc test
-  pdf(paste0(plot.dir, 'tukey_', attribute.name, '.pdf'), height=h, width=w)
-    tukey.matrix <- as.matrix(unlist(tukey$mcletters$Letters))
-    tukey.df <- data.frame(trt=row.names(tukey.matrix), M=tukey.matrix[,1])
-    Mean <- aggregate(x=list(means=temp$CS_Choice), by=list(trt=temp$Treatment), FUN=mean)
-    tukey.df <- merge(tukey.df, Mean)
-    tukey.df <- tukey.df[, c(1, 3, 2)]
-    tukey.df <- tukey.df[order(tukey.df$M),]
-    grid.table(tukey.df, rows=NULL)
-  ignore <- dev.off()
+  mcs <- summary(glht(model, linfct = mcp(Treatment="Tukey")))
+  tukey <- cld(mcs, level=0.05, decreasing=TRUE)
+  tukey.matrix <- as.matrix(unlist(tukey$mcletters$Letters))
+  tukey.df <- data.frame(trt=row.names(tukey.matrix), M=tukey.matrix[,1])
+  Mean <- aggregate(x=list(means=temp$CS_Choice), by=list(trt=temp$Treatment), FUN=mean)
+  tukey.df <- merge(tukey.df, Mean)
+  tukey.df <- tukey.df[, c(1, 3, 2)]
+  tukey.df <- tukey.df[order(tukey.df$M),]
 
   # find the greatest mean value for CS_Choice which has significance.
   # initialization: max.mean begins with lower possible value for mean of CS_Choice attribute
@@ -245,12 +222,44 @@ for (i in year.index:(CS.choice.index-1)) {  # CS.choice.index is the last one
                     max.mean=max.mean,
                     assumptions=a)
   treatments <- rbind(treatments, df)
+  
+  ## Charts
+  # barplot to show differences between treatments
+  temp2 <- aggregate(x=list(Quantity=temp$CS_Interest),
+                     by=list(CS_Interest=temp$CS_Interest, Treatment=temp$Treatment),
+                     FUN=length)
+  p <- ggplot(temp2, aes(fill=Treatment, y=Quantity, x=CS_Interest)) +
+    geom_bar(position="dodge", stat="identity") +
+    scale_fill_discrete(name='Treatment')
+  ggsave(paste0(plot.dir, 'plot_', attribute.name, '.pdf'), width=5, height=3)
+  
+  # anova table
+  pdf(paste0(plot.dir, 'anova_', attribute.name, '.pdf'), height=1, width=8)
+    grid.table(fit)
+  ignore <- dev.off()
+  
+  # Residuals charts
+  pdf(paste0(plot.dir, 'conditions_', attribute.name, '.pdf'))
+    # A warning here about the exclusion of the lack of fit chart. 
+    # This chart is not essencial to perform residuals analysis.
+    options(warn=-1)
+      residualPlots(model, layout= c(2, 1))
+    options(warn=0)
+    qqPlot(model)
+  ignore <- dev.off()
+  
+  # height and width adjustments based on trial and error
+  h <- 1.5 + 0.1 * length(unique(temp$Treatment))
+  w <- 1.5 + 0.1 * max(nchar(as.character(levels(temp$Treatment))))
+  pdf(paste0(plot.dir, 'tukey_', attribute.name, '.pdf'), height=h, width=w)
+    grid.table(tukey.df, rows=NULL)
+  ignore <- dev.off()
 }
 
 ## Attribute Selection using Factorial Analysis
 
 # Removing not-significant attributes using a top-down approach
-not_significant <- c(CS.response.index, year.index)
+not_significant <- c(CS.interest.index, year.index)
 temp <- poll.answers[, -not_significant]
 gotcha <- FALSE
 while(!gotcha | ncol(temp) <= 1) {
@@ -272,21 +281,27 @@ while(!gotcha | ncol(temp) <= 1) {
     not_significant <- c(not_significant, index)
     temp <- poll.answers[, -not_significant]
   } else {
-    print(fit)
-    break
+      pdf(paste0(plot.dir, 'attribute_selection.pdf'), height=6, width=9)
+        grid.table(fit)
+      ignore <- dev.off()
+      break
   }
 }
 
 ### Test interactions between Family_Approval and others attributes
+CS.interest.index <- match('CS_Interest', names(poll.answers))
+not_significant <- setdiff(not_significant, CS.interest.index)
+temp <- poll.answers[, -not_significant]
 Family.index <- match('Family_Approval', names(temp))
-temp <- temp[, c(setdiff(1:ncol(temp), Family.index), Family.index)]
-Family.index <- ncol(temp)
+temp <- temp[, c(setdiff(1:ncol(temp), c(Family.index, CS.interest.index)), Family.index, CS.interest.index)]
+Family.index <- match('Family_Approval', names(temp))
 CS.choice.index <- match('CS_Choice', names(temp))
+CS.interest.index <- match('CS_Interest', names(temp))
 for (i in 1:(CS.choice.index-1)) {
-  temp2 <- temp[, c(Family.index, i, CS.choice.index)]
+  temp2 <- temp[, c(Family.index, i, CS.choice.index, CS.interest.index)]
   FactorB.name <- names(temp2)[2]
   
-  trt <- paste0(names(temp2)[1], '_x_', names(temp2)[2])
+  Interaction.name <- paste0(names(temp2)[1], '_x_', names(temp2)[2])
   
   f <- as.formula(paste("CS_Choice ~ Family_Approval", "*", FactorB.name, "- 1"))
 
@@ -332,20 +347,16 @@ for (i in 1:(CS.choice.index-1)) {
     } else {
       a <- 'Not Granted'
     }
-
-    mcs <- summary(glht(model, linfct = mcp(I="Tukey")))
-    tukey <- cld(mcs, level=0.05, decreasing=TRUE)
     
     # tukey-kramer post hoc test
-    pdf(paste0(plot.dir, 'tukey_Family_Approval_x_', FactorB.name, '.pdf'), height=h, width=w)
+    mcs <- summary(glht(model, linfct = mcp(I="Tukey")))
+    tukey <- cld(mcs, level=0.05, decreasing=TRUE)
     tukey.matrix <- as.matrix(unlist(tukey$mcletters$Letters))
     tukey.df <- data.frame(trt=row.names(tukey.matrix), M=tukey.matrix[,1])
     Mean <- aggregate(x=list(means=temp2$CS_Choice), by=list(trt=temp2$I), FUN=mean)
     tukey.df <- merge(tukey.df, Mean)
     tukey.df <- tukey.df[, c(1, 3, 2)]
     tukey.df <- tukey.df[order(tukey.df$M),]
-    grid.table(tukey.df, rows=NULL)
-    ignore <- dev.off()
     
     # find the greatest mean value for CS_Choice which has significance.
     # initialization: max.mean begins with lower possible value for mean of CS_Choice attribute
@@ -361,15 +372,48 @@ for (i in 1:(CS.choice.index-1)) {
       }
     }
     
-    # pdf(paste0(plot.dir, 'interactions_', trt, '.pdf'))
-    #   interaction.plot(temp2[, 1], temp2[, 2], temp2[, 3], type='b')
-    # ignore <- dev.off()
-  
-    df <- data.frame(treatment=trt,
+    # adding significant interactions in control table
+    df <- data.frame(treatment=Interaction.name,
                      f.value=p_value,
                      max.mean=max.mean,
                      assumptions=a)
     treatments <- rbind(treatments, df)
+    
+    ## Charts
+    # barplot to show differences between treatments
+    temp3 <- aggregate(x=list(Quantity=temp2$CS_Interest),
+                       by=list(CS_Interest=temp2$CS_Interest, Interaction=temp2$I),
+                       FUN=length)
+    p <- ggplot(temp3, aes(fill=Interaction, y=Quantity, x=CS_Interest)) +
+      geom_bar(position="dodge", stat="identity") +
+      scale_fill_discrete(name='Interaction')
+    ggsave(paste0(plot.dir, 'plot_', Interaction.name, '.pdf'), width=8, height=5)
+    
+    # anova table
+    pdf(paste0(plot.dir, 'anova_', Interaction.name, '.pdf'), height=2, width=10)
+      grid.table(fit)
+    ignore <- dev.off()
+    
+    # Residuals charts
+    pdf(paste0(plot.dir, 'conditions_', Interaction.name, '.pdf'))
+      # A warning here about the exclusion of the lack of fit chart. 
+      # This chart is not essencial to perform residuals analysis.
+      options(warn=-1)
+        residualPlots(model, layout= c(2, 1))
+      options(warn=-1)
+      qqPlot(model)
+    ignore <- dev.off()
+    
+    # height and width adjustments based on trial and error
+    h <- 2.5 + 0.1 * length(unique(temp2$I))
+    w <- 1.5 + 0.1 * max(nchar(as.character(levels(temp2$I))))
+    pdf(paste0(plot.dir, 'tukey_', Interaction.name, '.pdf'), height=h, width=w)
+      grid.table(tukey.df, rows=NULL)
+    ignore <- dev.off()
+    
+    pdf(paste0(plot.dir, 'interactions_', Interaction.name, '.pdf'))
+      interaction.plot(temp2[, 1], temp2[, 2], temp2[, 3], type='b')
+    ignore <- dev.off()
   }
 }
 
