0da682c3a87ac8a3d30501c7e1a71f41a1e19e56
rnmourao <robertonunesmourao@yahoo.com.br>
2017-03-03 11:23:14 -0300

    simplify interactions analysis and adjusting plots

--- src/script.R
+++ src/script.R
@@ -22,7 +22,7 @@ cat('\n')
 rm(list=ls())
 
 #### Set working directory
-# setwd('/home/f8676628/Documentos/women_computer_science/src/')
+# setwd('/home/mourao/Documentos/women_computer_science/src/')
 plot.dir <- '../dexa/img/'
 
 # Get raw data
@@ -159,7 +159,7 @@ mean(poll.answers$CS.Choice)
 # a table to organize treatments by its means
 treatments <- data.frame(Attribute=NULL, F.Value=NULL, Assumptions=NULL, Max.Mean=NULL)
 
-## Attribute Selection using Factorial Analysis
+#################### Attribute Selection using Factorial Analysis ########################
 # Removing not-significant attributes using a top-down approach
 not.significant <- c(CS.interest.index)
 temp <- poll.answers[, -not.significant]
@@ -191,7 +191,7 @@ while(!gotcha | ncol(temp) <= 1) {
 # turning back with CS.Interest for additional analysis
 not.significant <- setdiff(not.significant, CS.interest.index)
 
-# Analyze individual attributes
+############################# Analyze individual attributes ##############################
 poll.answers <- poll.answers[, -not.significant]
 CS.choice.index <- match('CS.Choice', names(poll.answers))
 for (i in (CS.choice.index+1):ncol(poll.answers)) {  
@@ -292,13 +292,9 @@ for (i in (CS.choice.index+1):ncol(poll.answers)) {
   ggsave(paste0(plot.dir, 'plot.', attribute.name, '.pdf'), width=5, height=3)
   
   # Residuals charts
-  pdf(paste0(plot.dir, 'conditions.', attribute.name, '.pdf'))
-    # A warning here about the exclusion of the lack of fit chart. 
-    # This chart is not essencial to perform residuals analysis.
-    options(warn=-1)
-      residualPlots(model, layout= c(2, 1))
-    options(warn=0)
-    qqPlot(model)
+  pdf(paste0(plot.dir, 'assumptions.', attribute.name, '.pdf'))
+    par(mfrow=c(2,2))
+    plot(model)
   ignore <- dev.off()
   
   # anova table
@@ -336,29 +332,42 @@ for (i in 1:nrow(combinations)) {
   ## construct a new model to test only the interaction
   A.index <- match(combinations[i, 1], names(temp2))
   B.index <- match(combinations[i, 2], names(temp2))
-  temp2$I <- interaction(temp2[, A.index], temp2[, B.index], drop = TRUE, sep = ".x.")
+  temp2$Interaction <- interaction(temp2[, A.index], temp2[, B.index], drop = TRUE, sep = ".x.")
 
-  f <- as.formula(paste("CS.Choice ~",  combinations[i, 1], "*", combinations[i, 2], "- 1"))
-
-  # unbalanced designs need a different library to analyze interactions  
-  lst <- list(A="contr.sum", B="contr.sum")
-  names(lst) <- c(combinations[i, 1], combinations[i, 2])
-  model <- lm(formula=f, data=temp2, contrasts=lst)
+  #####  Test interaction
+  # Creating a model to test only the interaction
+  model <- lm(formula='CS.Choice ~ Interaction - 1', 
+              data=temp2, 
+              contrasts=list(Interaction='contr.sum'))
   fit <- Anova(model, type="III")
   fit.summary <- summary(model)
+  
+  # tukey-kramer post hoc test
+  mcs <- summary(glht(model, linfct=mcp(Interaction="Tukey")))
+  tukey <- cld(mcs, level=0.05, decreasing=TRUE)
+  tukey.matrix <- as.matrix(unlist(tukey$mcletters$Letters))
+  tukey.df <- data.frame(trt=row.names(tukey.matrix), M=tukey.matrix[,1])
+  Mean <- aggregate(x=list(means=temp2$CS.Choice), by=list(trt=temp2$Interaction), FUN=mean)
+  SD <- aggregate(x=list(SD=temp2$CS.Choice), by=list(trt=temp2$Interaction), FUN=sd)
+  tukey.df <- merge(tukey.df, Mean)
+  tukey.df <- merge(tukey.df, SD)
+  tukey.df <- tukey.df[, c(1, 3, 4, 2)]
+  tukey.df <- tukey.df[order(tukey.df$M),]
+  names(tukey.df) <- c(interaction.name, "Means", "Std.Dev", "Tukey")
   
   ## Charts
   # barplot to show differences between treatments
   temp3 <- aggregate(x=list(Quantity=temp2$CS.Interest),
-                     by=list(CS.Interest=temp2$CS.Interest, Interaction=temp2$I),
+                     by=list(CS.Interest=temp2$CS.Interest, Interaction=temp2$Interaction),
                      FUN=length)
   p <- ggplot(temp3, aes(fill=Interaction, y=Quantity, x=CS.Interest)) +
     geom_bar(position="dodge", stat="identity") +
     scale_fill_discrete(name=interaction.name)
   ggsave(paste0(plot.dir, 'plot.', interaction.name, '.pdf'), width=8, height=5)
   
-  # anova table main effects + interaction
-  pdf(paste0(plot.dir, 'anova.complete.', interaction.name, '.pdf'), height=2, width=10)
+  # anova table
+  pdf(paste0(plot.dir, 'anova.', interaction.name, '.pdf'), height=2, width=10)
+    rownames(fit)[1] <- interaction.name
     grid.table(fit)
   ignore <- dev.off()
   
@@ -373,43 +382,15 @@ for (i in 1:nrow(combinations)) {
                      trace.label=combinations[i, 2]) 
   ignore <- dev.off()
   
-  ##### Post hoc test for interactions
-  # Creating a model to test only the interaction
-  model <- lm(formula='CS.Choice ~ I - 1', data=temp2, contrasts=contr.sum)
-  fit <- Anova(model, type="III")
-  fit.summary <- summary(model)
-  
-  # tukey-kramer post hoc test
-  mcs <- summary(glht(model, linfct = mcp(I="Tukey")))
-  tukey <- cld(mcs, level=0.05, decreasing=TRUE)
-  tukey.matrix <- as.matrix(unlist(tukey$mcletters$Letters))
-  tukey.df <- data.frame(trt=row.names(tukey.matrix), M=tukey.matrix[,1])
-  Mean <- aggregate(x=list(means=temp2$CS.Choice), by=list(trt=temp2$I), FUN=mean)
-  SD <- aggregate(x=list(SD=temp2$CS.Choice), by=list(trt=temp2$I), FUN=sd)
-  tukey.df <- merge(tukey.df, Mean)
-  tukey.df <- merge(tukey.df, SD)
-  tukey.df <- tukey.df[, c(1, 3, 4, 2)]
-  tukey.df <- tukey.df[order(tukey.df$M),]
-  names(tukey.df) <- c(interaction.name, "Means", "Std.Dev", "Tukey")
-  
   ##  Charts
   # Residuals charts
-  pdf(paste0(plot.dir, 'conditions.', interaction.name, '.pdf'))
-    # A warning here about the exclusion of the lack of fit chart. 
-    # This chart is not essencial to perform residuals analysis.
-    options(warn=-1)
-      residualPlots(model, layout= c(2, 1))
-    options(warn=-1)
-    qqPlot(model)
+  pdf(paste0(plot.dir, 'assumptions.', interaction.name, '.pdf'))
+    par(mfrow=c(2,2))
+    plot(model)
   ignore <- dev.off()  
   
-  # anova table
-  pdf(paste0(plot.dir, 'anova.', interaction.name, '.pdf'), height=2, width=10)
-    grid.table(fit)
-  ignore <- dev.off()
-  
   # height and width adjustments based on trial and error
-  h <- 2.5 + 0.1 * length(unique(temp2$I))
+  h <- 2.5 + 0.1 * length(unique(temp2$Interaction))
   w <- 1.0 + 0.1 * sum(nchar(paste0(names(tukey.df))))
   pdf(paste0(plot.dir, 'tukey.', interaction.name, '.pdf'), height=h, width=w)
     grid.table(tukey.df, rows=NULL)
