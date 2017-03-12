1f1503265f0d1e84614edafad9142b277599767f
rnmourao <robertonunesmourao@yahoo.com.br>
2017-03-11 21:59:35 -0300

    removed No answers from dummy variables to ease the interpretation; adjustments on charts, replacing technique in text

--- src/apriori.R
+++ src/apriori.R
@@ -13,11 +13,8 @@ setwd('/home/mourao/Documentos/women_computer_science/src/')
 # Dependencies
 library(arules)
 library(arulesViz)
-library(caret)
-library(e1071)
 library(ggplot2)
 library(gridExtra)
-library(mlbench)
 library(readxl)
 
 # Pretty printing
@@ -34,7 +31,7 @@ plot.dir <- '../dexa/img/'
 ################################# Data Preparation ##########################################
 
 # Get raw data
-poll.answers <- read_excel('../data/raw.xlsx', sheet='answers', na='')
+poll.answers <- read_excel('../data/raw.xlsx', sheet='answers_2', col_types=rep("text", 38), na='')
 
 # Cleanup data
 poll.answers$Q1 <- NULL
@@ -67,6 +64,8 @@ poll.answers <- poll.answers[, c('CS.Interest', setdiff(names(poll.answers), 'CS
 
 cat('Female respondents: ', nrow(poll.answers), '\n\n')
 
+cat('Female interested in CS: ', nrow(poll.answers[poll.answers$CS.Interest=='Yes',]), '\n\n')
+
 ############################# Association Rule Mining #################################
 
 # Mining rules
@@ -74,17 +73,33 @@ cat('Female respondents: ', nrow(poll.answers), '\n\n')
 # APRIORI
 cs.rules = apriori(data = poll.answers, 
                    parameter = list(confidence = 0.5, maxtime = 300, maxlen=3), 
-                   appearance = list(rhs = "CS.Interest=Yes", default = "lhs"))
+                   appearance = list(rhs = list("CS.Interest=Yes",
+                                                "CS.Interest=No"), default = "lhs"))
 
 cs.rules.ordered <- sort(cs.rules, by = 'lift')
+
+rules.df <- cbind(as(cs.rules.ordered, "data.frame"))
+rules.df$lhs <- substr(rules.df$rules, 
+                       1, 
+                       regexpr(' =>', as.character(rules.df$rules), fixed=TRUE))
+rules.df$rhs <- substr(rules.df$rules, 
+                       regexpr('=>', as.character(rules.df$rules), fixed=TRUE) + 3, 
+                       nchar(as.character(rules.df$rules)))
+rules.df$rules <- NULL
+rules.df <- rules.df[, c(4, 5, 1, 2, 3)]
+
+# Saving rules on disk
+write(cs.rules.ordered, file='../data/apriori.csv', sep=";", row.names = FALSE)
 
 # plot
 pdf(paste0(plot.dir, 'plot.apriori.pdf'))
   plot(cs.rules.ordered)
 ignore <- dev.off()
 
-# Saving rules on disk
-write(cs.rules.ordered, file='../data/apriori.csv', sep=";", row.names = FALSE)
+# df
+pdf(paste0(plot.dir, 'apriori.pdf'), width=8, height=2)
+  grid.table(rules.df, rows=NULL)
+ignore <- dev.off()
 
 ############################################## Charts ###############################################
 ### Results per year 
@@ -165,6 +180,18 @@ p <- ggplot(data=interest.in.CS, aes(x='', y=Percentage, fill=CS.Interest)) +
   scale_fill_discrete(breaks=c('Yes', 'No', 'Maybe'))
 ggsave(paste0(plot.dir, 'InterestInCS.pdf'), width=7, height=2)
 
+
+### Show relation between CS.Interest and others Variables
+for (i in 2:ncol(poll.answers)) {
+  attribute.name <- names(poll.answers)[i]
+  temp <- aggregate(x=list(Quantity=poll.answers$CS.Interest),
+                    by=list(CS.Interest=poll.answers$CS.Interest, Treatment=poll.answers[, i]),
+                    FUN=length)
+  p <- ggplot(temp, aes(fill=Treatment, y=Quantity, x=CS.Interest)) +
+    geom_bar(position="dodge", stat="identity") +
+    scale_fill_discrete(name=attribute.name)
+  ggsave(paste0(plot.dir, 'plot.', attribute.name, '.pdf'), width=5, height=3)
+}
 
 ### Show relation between CS.Interest and others Variables
 for (i in 2:ncol(poll.answers)) {
